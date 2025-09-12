976876527ca8d5a9dac6ccd325ce761b7dfc1004
blinlin <38490701+blinlincom@users.noreply.github.com>
2025-09-12 13:26:29 +0800

    修复测试文件目录错误

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:max/main.dart';
+import 'package:max-flutter-app/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
